06d879d948912fc5274b1c9bff72025027744446
iblanco-s <113044030+iblanco-s@users.noreply.github.com>
2024-02-01 09:32:33 +0100

    added name trip

--- models/Trip.py
+++ models/Trip.py
@@ -7,6 +7,7 @@ class Trip(models.Model):
     _description = 'Trip Entity'
 
     id = fields.Integer(string='ID')
+    name = fields.Char(string='Name')
     trip_type = fields.Selection([('culture', 'Culture'), ('leisure', 'Leisure'), ('nature', 'Nature'),('sports','Sports')], string="Type")
     description = fields.Char(string='Description')
     customer = fields.Many2many('trandom.customer', string='Customer')
